cf719fa7200f41003deca2831df7824e69b4dec4
danielasakpa <danielasakpa@gmail.com>
2023-11-10 19:29:44 +0100

    Refactor: Rename Fetchers folder to Utils. Changed the folder name from 'Fetchers' to 'Utils' for better clarity and organisation.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 
+(function () {
+  var cors_api_host = 'web-production-1734.up.railway.app';
+  var cors_api_url = 'https://' + cors_api_host + '/';
+  var slice = [].slice;
+  var origin = window.location.protocol + '//' + window.location.host;
+  var open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    var args = slice.call(arguments);
+    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+      targetOrigin[1] !== cors_api_host) {
+      args[1] = cors_api_url + args[1];
+    }
+    return open.apply(this, args);
+  };
+})();
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
