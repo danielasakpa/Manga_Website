c0feef15500973c6d7fe69d3cf926e48284e9973
danielasakpa <danielasakpa@gmail.com>
2024-02-13 22:22:08 +0100

    refactor: Separate the SeeMore component from the MangaCards component

---
+++ src/components/SeeMore/SeeMore.js
@@ -0,0 +1,28 @@
+import { Oval } from 'react-loader-spinner'
+
+const SeeMore = ({ onClick, isLoadingNewMangas }) => {
+    console.log(isLoadingNewMangas)
+    return (
+        <div
+            className="md:mr-7 flex justify-center items-center bg-white text-black font-medium tracking-[0.3em] hover:bg-[#E40066] hover:text-white cursor-pointer h-[100%] w-[100px] bottom-2 left-14"
+            onClick={onClick}
+        >
+            {
+                isLoadingNewMangas ?
+                    <Oval
+                        visible={true}
+                        height="30"
+                        width="30"
+                        color="#000000"
+                        ariaLabel="oval-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />
+                    :
+                    <p className="text-center">See More</p>
+            }
+        </div>
+    );
+};
+
+export default SeeMore;
