8469f8d9e2631dacf6096da3b6980bf7e04779e7
danielasakpa <danielasakpa@gmail.com>
2024-02-16 22:41:55 +0100

    Used QueryClientProvider in the App component
    Moved the QueryClientProvider setup from index.js to the App component. This change includes setting refetchOnMount to false and retry option to true in order to enhance user experience and reduce initial load time.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from 'react'
 import MainRouter from './MainRouter'
 import { BrowserRouter, ScrollRestoration } from "react-router-dom";
 import { MangaProvider } from './context/MangaContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, Zoom } from 'react-toastify';
 
 const App = () => {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnmount: false,
+        refetchOnReconnect: true,
+        retry: true,
+      },
+    },
+  });
+
+
   return (
     <div>
       <MangaProvider>
         <BrowserRouter>
-          <MainRouter />
+          <QueryClientProvider client={queryClient}>
+            <MainRouter />
+          </QueryClientProvider>
           {/* <ScrollRestoration /> */}
         </BrowserRouter>
         <ToastContainer newestOnTop />
