6cc861ffb73ed9c4d7d72ed99a122c5fbb79cd0c
danielasakpa <danielasakpa@gmail.com>
2024-02-16 22:43:58 +0100

    Removed QueryClientProvider from index.js
    Moved the QueryClientProvider setup from index.js to the App component. This change improves the organization of the code and centralizes the configuration in the root component.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-// const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnmount: true,
-      refetchOnReconnect: true,
-      retry: false,
-    },
-  },
-});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <App />
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
