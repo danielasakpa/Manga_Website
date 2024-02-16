443493864bf781f71935a0f924b42aedf825fdc1
danielasakpa <danielasakpa@gmail.com>
2024-02-16 22:29:03 +0100

    Update query cache key in useManga hook
    Changed the key used in the query cache for the useManga hook from 'mangaDetails' to 'manga' to better reflect the purpose and content of the query.

--- src/hooks/manga/useManga.js
+++ src/hooks/manga/useManga.js
@@ -2,5 +2,5 @@ import { useQuery } from '@tanstack/react-query';
 import fetchManga from '../../API/manga/fetchManga';
 
 export function useManga(mangaId) {
-    return useQuery(['mangaDetails', mangaId], () => fetchManga(mangaId));
+    return useQuery(['manga', mangaId], () => fetchManga(mangaId));
 }
