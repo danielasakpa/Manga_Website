cabd4ffb927b8182a02d0b37772e9655e9ab1ffa
danielasakpa <danielasakpa@gmail.com>
2024-02-17 09:31:27 +0100

    Modified the useManga hook to include prefetching of manga chapters upon successful retrieval of manga details.

--- src/hooks/manga/useManga.js
+++ src/hooks/manga/useManga.js
@@ -1,6 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchManga from '../../API/manga/fetchManga';
+import fetchMangaChapters from '../../API/manga/fetchMangaChapters';
 
 export function useManga(mangaId) {
-    return useQuery(['manga', mangaId], () => fetchManga(mangaId));
+    const client = useQueryClient();
+
+    return useQuery(['manga', mangaId], () => fetchManga(mangaId),
+        {
+            onSuccess: (manga) => {
+                const languages = ['en'];
+                const mangaId = manga.id;
+                client.prefetchQuery(['mangaChapters', mangaId, languages], () => fetchMangaChapters(mangaId, languages));
+            }
+        });
 }
