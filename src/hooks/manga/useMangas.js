a5d6e29e1d77d1864bdfbc9e83b322e36d0f2f16
danielasakpa <danielasakpa@gmail.com>
2024-02-17 09:42:40 +0100

    Refactored the useMangas hook to streamline data prefetching. Removed unnecessary prefetch calls for cover images and statistics, focusing only on manga details. This improves efficiency and reduces unnecessary network requests.

--- src/hooks/manga/useMangas.js
+++ src/hooks/manga/useMangas.js
@@ -1,7 +1,5 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchMangas from "../../API/manga/fetchMangas";
-import fetchMangaCover from '../../API/manga/fetchMangaCover';
-import fetchMangaStatistics from '../../API/manga/fetchMangaStatistics';
 import fetchManga from '../../API/manga/fetchManga';
 
 export default function useMangas(type, order, limit, includedTags, excludedTags, page) {
@@ -14,8 +12,6 @@ export default function useMangas(type, order, limit, includedTags, excludedTags
                 const mangaId = manga.id;
                 const id = manga.id;
                 client.prefetchQuery(['manga', mangaId], () => fetchManga(mangaId));
-                client.prefetchQuery(['cover', id], () => fetchMangaCover(id));
-                client.prefetchQuery(['statistics', mangaId], () => fetchMangaStatistics(mangaId));
             })
         }
     });
