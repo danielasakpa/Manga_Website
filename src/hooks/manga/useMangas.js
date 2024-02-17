376a3193e0b45490a5e24c90f2e3ebb076e5caae
danielasakpa <danielasakpa@gmail.com>
2024-02-17 09:34:28 +0100

    Added prefetching of related data (manga, cover, and statistics) for each manga in the result during the onSuccess callback. This optimizes data fetching by proactively loading necessary data for improved user experience.

--- src/hooks/manga/useMangas.js
+++ src/hooks/manga/useMangas.js
@@ -1,8 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchMangas from "../../API/manga/fetchMangas";
+import fetchMangaCover from '../../API/manga/fetchMangaCover';
+import fetchMangaStatistics from '../../API/manga/fetchMangaStatistics';
+import fetchManga from '../../API/manga/fetchManga';
 
 export default function useMangas(type, order, limit, includedTags, excludedTags, page) {
+    const client = useQueryClient();
+
     return useQuery([type, page], () => fetchMangas(order, limit, includedTags, excludedTags, page), {
         keepPreviousData: true, // Ensure previous data is not discarded during pagination
+        onSuccess: (data) => {
+            data.data.map(manga => {
+                const mangaId = manga.id;
+                const id = manga.id;
+                client.prefetchQuery(['manga', mangaId], () => fetchManga(mangaId));
+                client.prefetchQuery(['cover', id], () => fetchMangaCover(id));
+                client.prefetchQuery(['statistics', mangaId], () => fetchMangaStatistics(mangaId));
+            })
+        }
     });
 }
