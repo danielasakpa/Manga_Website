ce674760091a483399f03a0c56dc5d7629eaddec
danielasakpa <danielasakpa@gmail.com>
2024-02-19 09:47:01 +0100

    chore: include all query parameters in useMangas dependency array
    In the useMangas hook, added all relevant query parameters (type, order, limit, includedTags, excludedTags, page) to the dependency array for useQuery. This ensures that the query is correctly invalidated when any of these parameters change.

--- src/hooks/manga/useMangas.js
+++ src/hooks/manga/useMangas.js
@@ -5,7 +5,7 @@ import fetchManga from '../../API/manga/fetchManga';
 export default function useMangas(type, order, limit, includedTags, excludedTags, page) {
     const client = useQueryClient();
 
-    return useQuery([type, page], () => fetchMangas(order, limit, includedTags, excludedTags, page), {
+    return useQuery([type, order, limit, includedTags, excludedTags, page], () => fetchMangas(order, limit, includedTags, excludedTags, page), {
         keepPreviousData: true, // Ensure previous data is not discarded during pagination
         onSuccess: (data) => {
             data.data.map(manga => {
