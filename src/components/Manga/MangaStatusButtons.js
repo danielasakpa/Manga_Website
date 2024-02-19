b982b9b4534f059d4c1b8f822876a2e4fa2d929d
danielasakpa <danielasakpa@gmail.com>
2024-02-19 09:31:09 +0100

    Rename variable 'item' to 'status' in MangaStatusButtons

--- src/components/Manga/MangaStatusButtons.js
+++ src/components/Manga/MangaStatusButtons.js
@@ -7,19 +7,19 @@ const MangaStatusButtons = ({ myList, selectedReading, handleReadingSelect, load
         <div className='mt-6'>
             <p className='text-[20px]'>My List</p>
             <div className='flex flex-wrap mt-4 border-box'>
-                {myList.map((item, index) => (
+                {myList.map((status, index) => (
                     <button
-                        className={`flex items-center justify-center md:basis-1/5 mr-1 md:mr-2 grow my-1 px-3 py-1 rounded-md font-semibold text-center text-[10px] md:text-[12px] tracking-[0.1em] ${item === "Remove form list" && selectedReading !== "Remove form list" && "bg-white text-red-500"}
-                            ${selectedReading === "Remove form list" && item === "Remove form list" ? "bg-red-500 text-white"
-                                : selectedReading === item
+                        className={`flex items-center justify-center md:basis-1/5 mr-1 md:mr-2 grow my-1 px-3 py-1 rounded-md font-semibold text-center text-[10px] md:text-[12px] tracking-[0.1em] ${status === "Remove form list" && selectedReading !== "Remove form list" && "bg-white text-red-500"}
+                            ${selectedReading === "Remove form list" && status === "Remove form list" ? "bg-red-500 text-white"
+                                : selectedReading === status
                                     ? "bg-blue-500 text-white"
                                     : "bg-gray-200 text-[#1F1F1F]"
                             }`}
                         key={index}
-                        disabled={loadingReadingList || selectedReading === item}
-                        onClick={() => handleReadingSelect(item)}
+                        disabled={loadingReadingList || selectedReading === status}
+                        onClick={() => handleReadingSelect(status)}
                     >
-                        {loadingReadingList && selectedReading === item ? (
+                        {loadingReadingList && selectedReading === status ? (
                             <Circles
                                 height="20"
                                 width="20"
@@ -31,7 +31,7 @@ const MangaStatusButtons = ({ myList, selectedReading, handleReadingSelect, load
                             />
                         )
                             :
-                            item
+                            status
                         }
                     </button>
                 ))}
