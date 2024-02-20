192073b84c5e0e8cc51ab1997b79a07ceb64e4dc
danielasakpa <danielasakpa@gmail.com>
2024-02-20 23:28:15 +0100

    refactor: format 'follows' label in MangaDetailsSection

--- src/components/Manga/MangaDetailsSection.js
+++ src/components/Manga/MangaDetailsSection.js
@@ -9,7 +9,12 @@ const MangaDetailsSection = ({ mangaDetails }) => {
                     key={index}
                     className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
                 >
-                    {detail.value}
+                    {
+                        detail.label === "follows" ?
+                            detail.value.toLocaleString()
+                            :
+                            detail.value
+                    }
                 </div>
             ))}
         </div>
