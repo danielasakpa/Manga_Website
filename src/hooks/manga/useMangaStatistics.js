1e93a0bb63549e099a3c11204bd9a025a6b33c6c
danielasakpa <danielasakpa@gmail.com>
2024-02-23 12:51:28 +0100

    Optimize useMangaStatistics hook
    - Added cacheTime: 0 to ensure fresh data on every request.

--- src/hooks/manga/useMangaStatistics.js
+++ src/hooks/manga/useMangaStatistics.js
@@ -2,5 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import fetchMangaStatistics from '../../API/manga/fetchMangaStatistics';
 
 export default function useMangaStatistics(mangaId) {
-    return useQuery(['statistics', mangaId], () => fetchMangaStatistics(mangaId));
+    return useQuery(['statistics', mangaId], () => fetchMangaStatistics(mangaId), {
+        cacheTime: 0
+    });
 }
