ec74a952045c30c7f231c01e8292c0bcb152ab82
danielasakpa <danielasakpa@gmail.com>
2024-02-24 23:33:17 +0100

    feat: enhance RelatedMangaSection with dynamic data loading
    - Introduce useRelatedManga hook to fetch related manga data
    - Display a skeleton loader while data is loading
    - Conditionally render the component based on data loading state
    - Minor code cleanup and organization

--- src/components/Manga/RelatedMangaSection.js
+++ src/components/Manga/RelatedMangaSection.js
@@ -1,29 +1,40 @@
 import HorizontalScrollMenu from '../HorizontalScrollMenu/HorizontalScrollMenu';
 import MangaCard from './MangaCard';
-
+import RelatedMangaSkeleton from './RelatedMangaSkeleton';
+import useRelatedManga from '../../hooks/manga/useRelatedManga';
 
 // Related Manga Section Component
-const RelatedMangaSection = ({ relatedManga, setIsLastItem }) => {
+const RelatedMangaSection = ({ mangaData, setIsLastItem }) => {
+
+    const { data, isLoading, isError } = useRelatedManga(mangaData);
+
     return (
-        relatedManga.length > 0 &&
-        <div className='bg-[#1F1F1F] mt-10'>
-            <div className='flex flex-col items-center justify-center px-[5px] md:px-[20px]'>
-                <h4 className='gradient-1 font-Kanit font-bold text-[20px] md:text-[35px] my-1 self-start'>RELATED MANGA</h4>
-                <HorizontalScrollMenu>
-                    {relatedManga?.map((manga) => (
-                        <div className='mr-5' key={manga.id}>
-                            <MangaCard
-                                itemId={manga.id}
-                                title={manga.id}
-                                key={manga.id}
-                                manga={manga}
-                                setIsLastItem={setIsLastItem}
-                            />
+        <>
+            {
+                isLoading ?
+                    <RelatedMangaSkeleton />
+                    :
+                    data.length > 0 &&
+                    <div className='bg-[#1F1F1F] mt-10'>
+                        <div className='flex flex-col items-center justify-center px-[5px] md:px-[20px]'>
+                            <h4 className='gradient-1 font-Kanit font-bold text-[20px] md:text-[35px] my-1 self-start'>RELATED MANGA</h4>
+                            <HorizontalScrollMenu>
+                                {data?.map((manga) => (
+                                    <div className='mr-5' key={manga.id}>
+                                        <MangaCard
+                                            itemId={manga.id}
+                                            title={manga.id}
+                                            key={manga.id}
+                                            manga={manga}
+                                            setIsLastItem={setIsLastItem}
+                                        />
+                                    </div>
+                                ))}
+                            </HorizontalScrollMenu>
                         </div>
-                    ))}
-                </HorizontalScrollMenu>
-            </div>
-        </div>
+                    </div>
+            }</>
+
     );
 };
 
