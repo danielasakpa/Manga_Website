19b3974565d3ed3e28e6f37a45ef4377b17beda0
danielasakpa <danielasakpa@gmail.com>
2024-02-25 23:42:28 +0100

    feat: Implement manga statistics integration
    - Added useMangaStatistics hook to fetch and display manga statistics (rating, follows).
    
    - Updated MangaDetailsSection to use the hook data.
    
    - Enhanced data fetching and presentation logic.

--- src/components/Manga/MangaDetailsSection.js
+++ src/components/Manga/MangaDetailsSection.js
@@ -1,23 +1,40 @@
+import useMangaStatistics from "../../hooks/manga/useMangaStatistics";
 
 
 // Manga Details Section Component
-const MangaDetailsSection = ({ mangaDetails }) => {
+const MangaDetailsSection = ({ id, mangaData }) => {
+    const { data: statistics, isLoading: isStatsLoading } = useMangaStatistics(id);
+
+    const { rating, follows } = statistics || {};
+
+    const mangaDetails = [
+        { label: 'Status', value: mangaData?.attributes?.status || "No Status" },
+        { label: 'Demographic', value: mangaData?.attributes?.publicationDemographic || "No Demographic".slice(0, 6) + "..." },
+        { label: 'Year', value: mangaData?.attributes?.year || "No date" },
+        { label: 'Rating', value: `${rating?.average?.toString().slice(0, 3)} ⭐` || "No rating" },
+        { label: 'follows', value: follows || 0 }
+    ];
     return (
-        <div className='flex flex-wrap xl:flex-nowrap justify-center text-center my-4'>
-            {mangaDetails.map((detail, index) => (
-                <div
-                    key={index}
-                    className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
-                >
-                    {
-                        detail.label === "follows" ?
-                            detail.value.toLocaleString()
-                            :
-                            detail.value
-                    }
+        <>
+            {
+                !isStatsLoading &&
+                <div className='flex flex-wrap xl:flex-nowrap justify-center text-center my-4'>
+                    {mangaDetails.map((detail, index) => (
+                        <div
+                            key={index}
+                            className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
+                        >
+                            {
+                                detail.label === "follows" ?
+                                    detail.value.toLocaleString()
+                                    :
+                                    detail.value
+                            }
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
+            }
+        </>
     );
 };
 
