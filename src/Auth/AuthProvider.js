0f222cc27b3ced5025f7fef74ced6c2b5b150157
danielasakpa <danielasakpa@gmail.com>
2024-03-08 03:14:01 +0100

    refactor: Improve authentication flow in AuthProvider
    - Restructured login logic to handle authentication more efficiently
    - Added check for existing token before making the initial authentication request
    - Implemented Axios request to '/login/success' for better handling of successful login
    - Utilized Cookies for managing authentication data and set proper conditions for token updates
    - Improved error handling and messages in showToast calls
    - Adjusted logout logic to handle Google authentication logout and server-side logout
    - Ensured secure cookie handling with proper configurations

--- src/Auth/AuthProvider.js
+++ src/Auth/AuthProvider.js
@@ -1,8 +1,9 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
 import showToast from '../utils/toastUtils';
+import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
 
@@ -10,18 +11,52 @@ const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem('token') || null);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const getUser = async () => {
+            if (!token) {
+                try {
+                    const response = await axios.get('https://manga-server-luxr.onrender.com/api/auth/login/success', {
+                        withCredentials: true,
+                    });
+
+                    showToast(response.data.message, 'success');
+
+                    if (response.status !== 200) {
+                        showToast('Authentication has failed!', 'error');
+                    }
+
+                    const authData = Cookies.get('auth_data');
+
+                    if (authData) {
+                        const { userId, token } = JSON.parse(authData.slice(2));
+                        localStorage.setItem('userId', userId);
+                        localStorage.setItem('token', token);
+                        localStorage.setItem("isGoogle", true);
+                        setToken(token);
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        };
+
+        getUser();
+    }, []);
+
     const login = async (userData) => {
         try {
             const response = await axios.post('https://manga-server-luxr.onrender.com/api/auth/login', userData);
-            const { userId, token } = response.data;
+            const { userId, message, token } = response.data;
             setToken(token);
+
+            showToast(message, 'success');
 
             localStorage.setItem('userId', userId);
             localStorage.setItem('token', token);
 
             navigate(-1 || '/');
         } catch (error) {
-            showToast('Login failed', "error");
+            showToast('Login failed', 'error');
         }
     };
 
@@ -29,6 +64,12 @@ const AuthProvider = ({ children }) => {
         setToken(null);
         localStorage.removeItem('token');
         localStorage.removeItem('userId');
+        if (localStorage.getItem('isGoogle')) {
+            localStorage.removeItem('isGoogle');
+            Cookies.remove('auth_data', { domain: 'localhost' });
+            window.open("https://manga-server-luxr.onrender.com/api/auth/logout", "_self");
+            return;
+        }
 
         navigate('/login');
     };
