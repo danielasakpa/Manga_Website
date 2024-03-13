b5b9ad8fa54013e98c1d3df0f8b1bfcf82ec1284
danielasakpa <danielasakpa@gmail.com>
2024-03-13 09:59:41 +0100

    Update useMangas hook to include Tags and utilize it in fetchMangas call

--- src/hooks/manga/useMangas.js
+++ src/hooks/manga/useMangas.js
@@ -1,11 +1,14 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchMangas from "../../API/manga/fetchMangas";
 import fetchManga from '../../API/manga/fetchManga';
+import tags from '../../assets/tags';
+
 
 export default function useMangas(type, order, limit, includedTags, excludedTags, page) {
     const client = useQueryClient();
 
-    return useQuery([type, includedTags, page], () => fetchMangas(order, limit, includedTags, excludedTags, page), {
+
+    return useQuery([type, includedTags, page], () => fetchMangas(tags, order, limit, includedTags, excludedTags, page), {
         keepPreviousData: true, // Ensure previous data is not discarded during pagination
         onSuccess: async (data) => {
             // Use Promise.all to parallelize fetching manga details
