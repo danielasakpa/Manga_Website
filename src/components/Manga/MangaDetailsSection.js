e8cff6e733f989a1d1b5c853042c0fbcd34dbc64
danielasakpa <danielasakpa@gmail.com>
2024-03-13 09:57:37 +0100

    Refactor MangaDetailsSection component to handle loading state more effectively

--- src/components/Manga/MangaDetailsSection.js
+++ src/components/Manga/MangaDetailsSection.js
@@ -17,22 +17,27 @@ const MangaDetailsSection = ({ id, mangaData }) => {
     return (
         <>
             {
-                !isStatsLoading &&
-                <div className='flex flex-wrap xl:flex-nowrap justify-center text-center my-4'>
-                    {mangaDetails.map((detail, index) => (
-                        <div
-                            key={index}
-                            className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
-                        >
-                            {
-                                detail.label === "follows" ?
-                                    detail.value.toLocaleString()
-                                    :
-                                    detail.value
-                            }
-                        </div>
-                    ))}
-                </div>
+                !isStatsLoading && statistics ?
+                    <div className='flex flex-wrap xl:flex-nowrap justify-center text-center my-4'>
+                        {mangaDetails.map((detail, index) => (
+                            <div
+                                key={index}
+                                className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
+                            >
+                                {
+                                    detail.label === "follows" ?
+                                        detail.value.toLocaleString()
+                                        :
+                                        detail.value
+                                }
+                            </div>
+                        ))}
+                    </div> :
+                    <div className="flex flex-wrap xl:flex-nowrap justify-center text-center my-4">
+                        {[...Array(5)].map((_, index) => (
+                            <div key={index} className="animate-pulse basis-1/3 mb-4 md:mb-0 px-4 py-3 w-full md:w-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.3em] cursor-pointer bg-white text-[#1F1F1F]"></div>
+                        ))}
+                    </div>
             }
         </>
     );
