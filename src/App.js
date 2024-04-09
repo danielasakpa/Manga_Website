4fdd359368f9927b4d015aa3d5059c9bfea672a2
danielasakpa <danielasakpa@example.com>
2024-04-09 07:02:28 -0700

    Refactor App component to remove  MangaContext provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MainRouter from './MainRouter'
-import { BrowserRouter, ScrollRestoration } from "react-router-dom";
-import { MangaProvider } from './context/MangaContext';
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
@@ -20,14 +19,12 @@ const App = () => {
 
   return (
     <div>
-      <MangaProvider>
         <BrowserRouter>
           <QueryClientProvider client={queryClient}>
             <MainRouter />
           </QueryClientProvider>
           {/* <ScrollRestoration /> */}
         </BrowserRouter>
-      </MangaProvider>
     </div>
   )
 }
