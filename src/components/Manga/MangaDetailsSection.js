4576a78d8d0b30eb82693807edc76de4444fcb59
danielasakpa <danielasakpa@gmail.com>
2024-04-14 17:52:45 -0700

    Update MangaDetailsSection component: - Refactored component to receive 'statistics' and 'isStatsLoading' as props instead of fetching them directly. - Improved styles for better responsiveness and user experience. - Adjusted spacing and alignment of manga details elements. - Minor code formatting and cleanup.

--- src/components/Manga/MangaDetailsSection.js
+++ src/components/Manga/MangaDetailsSection.js
@@ -1,9 +1,7 @@
-import useMangaStatistics from "../../hooks/manga/useMangaStatistics";
 
 
 // Manga Details Section Component
-const MangaDetailsSection = ({ id, mangaData }) => {
-    const { data: statistics, isLoading: isStatsLoading } = useMangaStatistics(id);
+const MangaDetailsSection = ({ id, mangaData, statistics, isStatsLoading }) => {
 
     const { rating, follows } = statistics || {};
 
@@ -18,11 +16,11 @@ const MangaDetailsSection = ({ id, mangaData }) => {
         <>
             {
                 !isStatsLoading && statistics ?
-                    <div className='flex flex-wrap xl:flex-nowrap justify-center text-center my-4'>
+                    <div className='flex flex-wrap justify-center my-4 text-center xl:flex-nowrap gap-y-1'>
                         {mangaDetails.map((detail, index) => (
                             <div
                                 key={index}
-                                className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
+                                className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-x-[1.5px] border-opacity-30 md:mx-[2px] border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
                             >
                                 {
                                     detail.label === "follows" ?
@@ -33,9 +31,9 @@ const MangaDetailsSection = ({ id, mangaData }) => {
                             </div>
                         ))}
                     </div> :
-                    <div className="flex flex-wrap xl:flex-nowrap justify-center text-center mt-4">
+                    <div className="flex flex-wrap justify-center mt-4 text-center xl:flex-nowrap gap-y-1">
                         {[...Array(5)].map((_, index) => (
-                            <div key={index} className="animate-pulse basis-1/3 px-4 py-3.5 md:py-4 w-full md:w-1/3 border-2 border-[#1F1F1F] font-medium tracking-[0.3em] cursor-pointer bg-white text-[#1F1F1F]"></div>
+                            <div key={index} className="animate-pulse basis-1/3 px-4 py-3.5 md:py-4 w-full md:w-1/3 border-1 md:mx-[2px] border-[#1F1F1F] font-medium tracking-[0.3em] cursor-pointer bg-white text-[#1F1F1F]"></div>
                         ))}
                     </div>
             }
