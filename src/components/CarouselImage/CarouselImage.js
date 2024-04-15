ff4c54cd375925cdcb85c3520f5cdffd58bc4198
danielasakpa <danielasakpa@gmail.com>
2024-04-14 18:36:23 -0700

    Refactor CarouselImage component: - Updated PROXY_SERVER_URL to 'https://yuki-proxy-server.netlify.app'. - Updated the API call to use the updated useMangaChapters hook with new parameters. - Adjusted the background image rendering for better performance and added lazy loading attributes to images. - Improved image rendering by setting fetch priority to 'high'. - Enhanced conditional rendering and error handling for cover and chapter data. - Adjusted the layout and styling for better readability and responsiveness. - Improved the use of absolute positioning for elements within the carousel image.

--- src/components/CarouselImage/CarouselImage.js
+++ src/components/CarouselImage/CarouselImage.js
@@ -6,26 +6,35 @@ import { Link } from "react-router-dom";
 
 const CarouselImage = ({ manga }) => {
     const { data: coverFilename, isLoading: isCoverLoading, isError: isCoverError, error: coverError } = useMangaCover(manga.id);
-    const { data: chaptersData, isLoading: isChaptersLoading, isError: ischaptersError, error: chaptersError } = useMangaChapters(manga.id);
+    const {
+        data: chaptersData,
+        isLoading: isChaptersLoading,
+        isError: ischaptersError,
+        error: chaptersError,
+    } = useMangaChapters(manga.id, ["en"], 100, 0, true);
 
     const [sortedChapters, setSortedChapters] = useState(null);
 
-    const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+    const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
     useEffect(() => {
-        if (chaptersData && chaptersData.data && chaptersData.data.length > 0) {
-            const filteredChapters = chaptersData.data.filter(
+        if (chaptersData && chaptersData && chaptersData.length > 0) {
+            const filteredChapters = chaptersData.filter(
                 (chapter, index, self) => {
+                    // Find the index of the first occurrence of the chapter number in the array
                     const firstIndex = self.findIndex(
                         (c) => c.attributes.chapter === chapter.attributes.chapter
                     );
+
+                    // Keep only the chapter if its index matches the first occurrence index
                     return index === firstIndex;
                 }
             );
 
             const sortedChapters = filteredChapters.sort((a, b) => {
                 return (
-                    parseInt(a.attributes.chapter, 10) - parseInt(b.attributes.chapter, 10)
+                    parseInt(a.attributes.chapter, 10) -
+                    parseInt(b.attributes.chapter, 10)
                 );
             });
 
@@ -44,16 +53,15 @@ const CarouselImage = ({ manga }) => {
                     <CarouselImageSkeleton />
                     :
                     <div
-                        className="w-full flex-shrink-0 relative flex justify-left pt-[110px] lg:pt-[100px] pl-2 md:pl-4 pr-4 lg:pr-0 lg:pl-[70px] xl:pl-[120px] bg-gray-600 md:hover:bg-cover cursor-pointer md:hover:bg-top bg-cover md:bg-contain bg-top md:bg-center bg-no-repeat"
-                        style={{
-                            backgroundImage: `${!isCoverLoading && `url(${PROXY_SERVER_URL}/images/${manga.id}/${encodeURIComponent(imageUrl)})`}`,
-                        }}
+                        className="w-full flex-shrink-0 relative flex justify-left pt-[110px] lg:pt-[100px] pl-2 md:pl-4 pr-4 lg:pr-0 lg:pl-[70px] xl:pl-[120px] bg-gray-600"
                     >
-                        <div className="absolute inset-0 bg-[#1F1F1F] bg-opacity-50 rounded-lg" />
-                        <div className="absolute flex items-center px-2 py-1 font-semibold tracking-[0.1em] bg-white text-gray-800 space-x-2 bg-white rounded-md top-4 right-4">
+                        <img decoding='async' fetchPriority='high' className='absolute w-full bg-cover md:bg-contain md:w-[300px] h-full z-10 inset-0 left-1/2 transform -translate-x-1/2' loading='eager' src={`${!isCoverLoading && `${PROXY_SERVER_URL}/images/${manga.id}/${encodeURIComponent(imageUrl)}`}`} alt='' />
+                        <img decoding='async' fetchPriority='high' className="hidden md:block absolute w-full inset-0 bg-[#1F1F1F] bg-cover bg-center opacity-20 rounded-lg" loading='eager' src={`${!isCoverLoading && `${PROXY_SERVER_URL}/images/${manga.id}/${encodeURIComponent(imageUrl)}`}`} alt='' />
+                        <div className='absolute inset-0 z-20 w-full bg-black bg-center bg-cover rounded-lg opacity-40' />
+                        <div className="absolute z-30 flex items-center px-2 py-1 font-semibold tracking-[0.1em] bg-white text-gray-800 space-x-2 rounded-md top-4 right-4">
                             <span className="">{manga.type.toUpperCase()}</span>
                         </div>
-                        <div className="z-10 lg:w-[60%] ml-3 sm:ml-5 lg:ml-0 ">
+                        <div className="z-30 lg:w-[60%] ml-3 sm:ml-5 lg:ml-0">
                             <h1 className="text-white font-semibold text-[30px] md:text-[40px] leading-9 lg:leading-10 mb-2">
                                 {manga.attributes.title.en
                                     ? manga.attributes.title.en.split(" ").slice(0, 3).join(" ")
@@ -72,17 +80,17 @@ const CarouselImage = ({ manga }) => {
                         </div>
                         {
                             isChaptersLoading ?
-                                <div className="absolute bottom-4 left-4 font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
+                                <div className="absolute bottom-4 left-4 z-30 font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
                                     Loading
                                 </div>
                                 :
                                 sortedChapters === null ? (
-                                    <div className="absolute bottom-4 left-2 md:left-4 text-[13px] md:text-[16px] font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
+                                    <div className="absolute bottom-4 left-2 z-30 md:left-4 text-[13px] md:text-[16px] font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
                                         No Chapter
                                     </div>
                                 ) : (
-                                    <div className="absolute bottom-4 left-2 md:left-4 font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
-                                        Chapter {sortedChapters[sortedChapters.length - 1].attributes.chapter}
+                                    <div className="absolute bottom-4 left-2 z-30 md:left-4 font-semibold tracking-[0.4em] bg-white text-gray-800 px-10 py-2 rounded-lg">
+                                        Chapter {sortedChapters[sortedChapters.length - 1]?.attributes.chapter}
                                     </div>
                                 )
                         }
