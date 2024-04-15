72556d3c5e2439f4a1ab5082c83d157fe682819b
danielasakpa <danielasakpa@gmail.com>
2024-04-14 17:54:16 -0700

    Update MangaImageAndDescriptionSection component: - Updated PROXY_SERVER_URL to 'https://yuki-proxy-server.netlify.app'. - Refactored component to receive 'statistics' and 'isStatsLoading' as props instead of fetching them directly. - Improved styles for better responsiveness and user experience. - Adjusted conditional rendering of cover image loading state. - Minor code formatting and cleanup.

--- src/components/Manga/MangaImageAndDescriptionSection.js
+++ src/components/Manga/MangaImageAndDescriptionSection.js
@@ -5,11 +5,11 @@ import MangaDetailsSection from '../../components/Manga/MangaDetailsSection';
 import useMangaCover from "../../hooks/manga/useMangaCover";
 
 // Manga Image and Description Section Component
-const MangaImageAndDescriptionSection = ({ id, mangaData, selectedReading, loadingReadingList, handleReadingSelect }) => {
+const MangaImageAndDescriptionSection = ({ id, mangaData, statistics, isStatsLoading, selectedReading, loadingReadingList, handleReadingSelect }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
     const { data: coverFilename, isLoading: isCoverLoading } = useMangaCover(id);
 
-    const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+    const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
 
     const authorRelationship = mangaData.relationships.find(
@@ -35,15 +35,18 @@ const MangaImageAndDescriptionSection = ({ id, mangaData, selectedReading, loadi
 
     return (
         <>
-            <MangaDetailsSection id={id} mangaData={mangaData} />
+            <MangaDetailsSection id={id} mangaData={mangaData} statistics={statistics} isStatsLoading={isStatsLoading} />
 
-            <div className='flex flex-col items-center justify-between mt-3 md:mt-8 mb-6 md:flex-row md:items-start md:space-x-10'>
-                <div className='h-[100%] w-[90%] md:w-[400px] shadow-yellow rounded-md'>
-                    {
-                        !isCoverLoading &&
-                        <img src={`${PROXY_SERVER_URL}/images/${id}/${encodeURIComponent(coverFilename)}`} alt={mangaData?.attributes.title.en} className="bg-auto bg-top w-full h-full rounded-md" />
-                    }
-                </div>
+            <div className='flex flex-col items-center justify-between mt-3 mb-6 md:mt-8 md:flex-row md:items-start md:space-x-10'>
+                {
+                    isCoverLoading ?
+                        <div className='h-[100%] w-[90%] md:w-[max-content] shadow-yellow rounded-md mx-auto mt-8 mb-12 sm:m-0'>
+                            <div className="animate-pulse bg-white bg-opacity-40 h-[400px] md:h-[310px] lg:h-[450px] min-w-[250px] lg:w-[273px] rounded-md" />
+                        </div> :
+                        <div className='h-[350px] md:h-[310px] lg:h-[450px] w-[90%] bg-white bg-opacity-40 md:w-[400px] mt-6 sm:m-0 shadow-yellow rounded-md'>
+                            <img src={`${PROXY_SERVER_URL}/images/${id}/${encodeURIComponent(coverFilename)}`} alt={mangaData?.attributes.title.en} decoding='async' fetchPriority='high' loading='eager' className="w-full h-full bg-top bg-auto rounded-md" />
+                        </div>
+                }
                 <div className='mt-4 md:mt-0 w-[95%] md:w-full'>
                     <h2 className='mb-3 mt-5 md:mt-0 text-[25px] md:text-[30px] font-bold text-start text-white'>{mangaData?.attributes.title.en}</h2>
                     <div className="mb-3 mt-5 md:mt-0 text-[14px]">
