944b920373162a65c2f9eea0f7d6562cce00ec42
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:23:31 -0700

    refactor: Update proxy server endpoint and error handling - Added error handling to check for undefined response data and throw an error if no data is received.

--- src/API/manga/fetchMangaStatistics.js
+++ src/API/manga/fetchMangaStatistics.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
 export default async function fetchMangaStatistics(mangaId) {
-    const response = await axios({
-        method: 'get',
-        url: `${PROXY_SERVER_URL}/api/statistics/manga/${mangaId}`,
-        withCredentials: true,
-    });
-    const { rating, follows } = response.data.statistics[mangaId];
-    return { rating, follows };
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `${PROXY_SERVER_URL}/api/v1/statistics/manga/${mangaId}`,
+        });
+
+        // Check if response data is undefined or does not contain required statistics
+        if (!response.data || !response.data.statistics || !response.data.statistics[mangaId]) {
+            throw new Error('No data received or incomplete statistics from fetchMangaStatistics');
+        }
+
+        const { rating, follows } = response.data.statistics[mangaId];
+        return { rating, follows };
+    } catch (error) {
+        console.error(`Error fetching manga statistics for manga ${mangaId}:`, error);
+        throw error; // Rethrow the error to be caught by the caller
+    }
 }
