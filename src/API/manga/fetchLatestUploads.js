5a7b1138ed743bcc12dc99d81d4a9780e7a3b6ef
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:09:44 -0700

    feat: created fetchLatestUploads API function to fetch the latest uploaded manga chapters data from mangaDex API

---
+++ src/API/manga/fetchLatestUploads.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
+
+function groupChaptersByMangaID(chapters) {
+  const groupedChapters = [];
+
+  chapters.forEach(chapter => {
+    const mangaID = chapter.relationships.find(rel => rel.type === 'manga').id;
+    const existingGroup = groupedChapters.find(group => group.mangaID === mangaID);
+    if (existingGroup) {
+      existingGroup.chapters.push(chapter);
+    } else {
+      groupedChapters.push({
+        mangaID,
+        chapters: [chapter]
+      });
+    }
+  });
+
+  return groupedChapters;
+}
+
+
+
+export default async function fetchLatestUploads(order, limit, includes = [], page = 0) {
+  try {
+    const finalOrderQuery = {};
+
+    if (
+      Object.keys(order).length !== 0 &&
+      order.hasOwnProperty("") === false
+    ) {
+      for (const [key, value] of Object.entries(order)) {
+        finalOrderQuery[`order[${key}]`] = value;
+      }
+    }
+
+    const params = {
+      includes,
+      ...finalOrderQuery,
+      contentRating: ["safe", "suggestive", "erotica", "pornographic"],
+      limit,
+      offset: page * limit,
+    };
+
+    const response = await axios.get(`${PROXY_SERVER_URL}/api/v1/chapter`, {
+      params: params,
+    });
+
+    const { data, ...otherInfo } = response.data;
+
+    const chaptersGroupedByMangaID = groupChaptersByMangaID(
+      data
+    );
+
+    return {
+      data: chaptersGroupedByMangaID,
+      otherInfo,
+    };
+  } catch (error) {
+    throw new Error('Error fetching manga chapters');
+  }
+}
