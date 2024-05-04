a0699dc2399163aef0a3f959319c6ede1ca7cc31
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:03:59 -0700

    refactor: changed the behavior option of the scrollTo method in MainRouter component

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -25,7 +25,7 @@ function MainRouter() {
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant", // Optional if you want to skip the scrolling animation
+      behavior: "smooth", 
     });
   }, [location.pathname]);
 
