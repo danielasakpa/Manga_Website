38f09ed848d45ada5e69dfd3887982bb566113cf
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:15:39 -0700

    refactor: Update proxy server endpoint and error handling - Modified the fetchMangaByTitle function to include additional query parameters for fetching manga details. - Added error handling to check for undefined response data and throw an error if no data is received.

--- src/API/manga/fetchMangaByTitle.js
+++ src/API/manga/fetchMangaByTitle.js
@@ -1,16 +1,43 @@
 import axios from 'axios';
 
-const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
-export default async function fetchMangaByTitle(title) {
-  const response = await axios({
-    method: 'GET',
-    url: `${PROXY_SERVER_URL}/search/manga`,
-    withCredentials: true,
-    params: {
-      title: title
+export default async function fetchMangaByTitle(title, limit, page) {
+  try {
+    const order = {
+      followedCount: "desc",
+      rating: "desc",
+    };
+
+    const finalOrderQuery = {};
+
+    // Check if order object is not empty and does not have a prototype property
+    if (Object.keys(order).length !== 0 && !Object.prototype.hasOwnProperty.call(order, "")) {
+      // Loop through order object and construct finalOrderQuery
+      for (const [key, value] of Object.entries(order)) {
+        finalOrderQuery[`order[${key}]`] = value;
+      }
     }
-  });
+    
+    const response = await axios({
+      method: "GET",
+      url: `${PROXY_SERVER_URL}/api/v1/manga`,
+      params: {
+        title: title,
+        ...finalOrderQuery,
+        limit,
+        offset: page * limit,
+      },
+    });
 
-  return response.data.data;
+    // Check if response data is undefined
+    if (!response.data) {
+      throw new Error('No valid data received from fetchMangaByTitle');
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching manga by title:', error);
+    throw error; // Rethrow the error to be caught by the caller
+  }
 }
