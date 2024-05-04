868a2b064f6b4f9d01a363d08a914c7adc5818f4
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:19:58 -0700

    refactor: Update proxy server endpoint and error handling - Added error handling to check for undefined response data and throw an error if no data is received.

--- src/API/manga/fetchMangaCover.js
+++ src/API/manga/fetchMangaCover.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
 export default async function fetchMangaCover(mangaId) {
-    const response = await axios({
-        method: 'get',
-        url: `${PROXY_SERVER_URL}/api/cover?manga[]=${mangaId}`,
-        withCredentials: true,
-    });
-    const cover = response.data.data;
-    return cover ? cover[0].attributes.fileName : null;
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `${PROXY_SERVER_URL}/api/v1/cover?manga[]=${mangaId}`,
+        });
+
+        // Check if response data is undefined or empty
+        if (!response.data || !response.data.data || response.data.data.length === 0) {
+            throw new Error('No valid cover data received from fetchMangaCover');
+        }
+
+        const cover = response.data.data;
+        return cover[0].attributes.fileName;
+    } catch (error) {
+        console.error(`Error fetching manga cover for manga ${mangaId}:`, error);
+        throw error; // Rethrow the error to be caught by the caller
+    }
 }
