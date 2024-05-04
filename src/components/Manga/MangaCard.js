f08fa8bc7790e4d2572af718f773530dd6cae0e7
danielasakpa <danielasakpa@gmail.com>
2024-05-04 16:51:48 -0700

    refactor: Refactor MangaCard component
    - Utilized useCallback hook to memoize the truncateTitle function for better performance.
    - Replaced the img tag with the reusable Image component for better code organization and reusability.
    - Implemented motion animations from Framer Motion for smoother transitions on hover.
    - Applied consistent styling across different screen sizes for better responsiveness.

--- src/components/Manga/MangaCard.js
+++ src/components/Manga/MangaCard.js
@@ -1,132 +1,129 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from "react";
 import useMangaCover from "../../hooks/manga/useMangaCover";
 import useMangaStatistics from "../../hooks/manga/useMangaStatistics";
-import { VisibilityContext } from 'react-horizontal-scrolling-menu';
-import MangaCardSkeleton from './MangaCardSkeleton';
+import { VisibilityContext } from "react-horizontal-scrolling-menu";
+import MangaCardSkeleton from "./MangaCardSkeleton";
 import { Link } from "react-router-dom";
+import Image from "../Image/Image";
+import { motion as m } from "framer-motion";
 
-
-const MangaCard = React.memo(({ manga, setIsLastItem = () => { return false } }) => {
+const MangaCard = React.memo(
+  ({
+    manga,
+    setIsLastItem = () => {
+      return false;
+    },
+  }) => {
     const { id, attributes } = manga;
-
-    const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
     const { isLastItemVisible } = useContext(VisibilityContext);
 
     useEffect(() => {
-        setIsLastItem(isLastItemVisible);
-    }, [isLastItemVisible])
-
+      setIsLastItem(isLastItemVisible);
+    }, [isLastItemVisible]);
 
     const [isHovered, setIsHovered] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
 
     const handleMouseEnter = () => {
-        setIsHovered(true);
-
+      setIsHovered(true);
     };
 
     const handleMouseLeave = () => {
-        setIsHovered(false);
+      setIsHovered(false);
     };
 
     const handleClick = () => {
-        setIsClicked(!isClicked);
+      setIsClicked(!isClicked);
     };
 
-    const { data: coverFilename, isLoading: isCoverLoading, isError: isCoverError, error: coverError } = useMangaCover(id);
-    const { data: statistics, isLoading: isStatsLoading, isError: isStatsError, error: statsError } = useMangaStatistics(id);
+    const {
+      data: coverFilename,
+      isLoading: isCoverLoading,
+      isError: isCoverError,
+      error: coverError,
+    } = useMangaCover(id);
+    const {
+      data: statistics,
+      isLoading: isStatsLoading,
+      isError: isStatsError,
+      error: statsError,
+    } = useMangaStatistics(id);
+
+    const truncateTitle = useCallback((title) => {
+      // Check if title is provided
+      if (!title || typeof title !== "string") {
+        return "No Title";
+      }
+
+      // Split the title into words
+      const words = title.split(" ");
+
+      // Initialize variables
+      let truncatedTitle = "";
+      let count = 0;
+
+      // Loop through the words
+      for (const word of words) {
+        // Truncate the word if it exceeds 7 characters
+        truncatedTitle += word.length > 7 ? word.slice(0, 7) : word;
+        truncatedTitle += " ";
+        count++;
+
+        // Check if we have reached the maximum word count or if the current word exceeds 7 characters
+        if (count >= 3 || word?.length > 7) {
+          break;
+        }
+      }
+
+      // Remove the trailing space
+      truncatedTitle = truncatedTitle.trim();
+
+      // Add ellipsis if there are more than three words
+      if (words.length >= 3) {
+        truncatedTitle += "...";
+      }
+
+      return truncatedTitle;
+    }, []);
 
     if (isStatsLoading || isStatsError) {
-        return (
-            <MangaCardSkeleton />
-        );
+      return <MangaCardSkeleton />;
     }
-
 
     const imageUrl = coverFilename;
 
     const { rating, follows } = statistics;
 
     const getTitle = (language) => {
-        const altTitle = attributes.altTitles.find((title) => title[language]);
-        if (altTitle) {
-            return altTitle[language];
-        } else {
-            return attributes.title.en;
-        }
+      const altTitle = attributes.altTitles.find((title) => title[language]);
+      if (altTitle) {
+        return altTitle[language];
+      } else {
+        return attributes.title.en;
+      }
     };
 
-    function truncateTitle(title) {
-      if (!title) {
-        return;
-      }
-
-      // Split the title into words
-      const words = title?.split(" ");
-
-      let truncatedTitle = "";
-      let count = 0;
-
-      if (words?.length < 2) {
-        return (
-          words[0].slice(0, 10) +
-          (words[0].slice(0, 10).length > 10 ? "..." : "")
-        );
-      }
-
-      // Loop through the words
-      for (const word of words) {
-        // Check if adding the current word will exceed 10 characters
-        if (word?.length > 7) {
-          // If adding this word would exceed 10 characters, add it and stop concatenating
-          truncatedTitle += word?.slice(0, 7);
-          break;
-        }
-
-        // Concatenate the word to the shortened title
-        truncatedTitle += word + " ";
-        count++;
-
-        // Check if we have already added three words
-        if (count >= 3) {
-          // If we have added three words, stop concatenating
-          break;
-        }
-      }
-
-      // Remove the trailing space
-      truncatedTitle = truncatedTitle.trim() + (words?.length >= 3 ? "..." : "");
-
-      return truncatedTitle;
-    }
-
-    const title = attributes.title.en || getTitle('en') || getTitle('ja');
-
-    
+    const title = attributes.title.en || getTitle("en") || getTitle("ja");
 
     return (
-      <div
-        className={`relative rounded-md md:rounded-lg cursor-pointer w-[120px] h-[170px] xm:w-[160px] xm:h-[200px] md:w-[300px] md:h-[300px] lg:w-[300px] lg:h-[530px]  overflow-hidden shadow-lg bg-white transform transition-transform `}
+      <m.div
+        className={`relative rounded-[4px] cursor-pointer w-[120px] h-[170px] xm:w-[160px] xm:h-[200px] md:w-[300px] md:h-[300px] lg:w-[280px] lg:h-[530px]  overflow-hidden shadow-lg bg-white transform transition-transform `}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         onClick={handleClick}
+        whileHover={{ scale: 1.1 }}
       >
         <div className="flex justify-center items-center h-[200px] w-[100%] md:h-[300px] overflow-hidden">
-          {isCoverLoading || isCoverError ? (
-            <div className="h-[200px] w-[100%] md:h-[300px] bg-gray-200 animate-pulse" />
-          ) : (
-            <img
-              src={`${PROXY_SERVER_URL}/images/${id}/${encodeURIComponent(
-                imageUrl
-              )}`}
-              alt={attributes.title.en}
-              decoding="async"
-              fetchPriority="high"
-              loading="eager"
-              className="h-[200px] w-[100%] md:h-[300px] object-fill md:object-cover transition ease-in-out delay-150 lg:hover:-translate-y-1 lg:hover:scale-110 cursor-pointer duration-300"
-            />
-          )}
+          <Image
+            id={id}
+            coverFilename={imageUrl}
+            decoding="async"
+            fetchPriority="high"
+            loading="lazy"
+            className="h-[200px] w-[100%] md:h-[300px] text-[10px] md:text-[15px] object-fill md:object-cover transition ease-in-out delay-150 lg:hover:-translate-y-1 lg:hover:scale-110 cursor-pointer duration-300"
+            size={512}
+          />
         </div>
         <div
           className={`absolute top-0 left-0 right-0 bottom-0 hidden lg:flex bg-[#1F1F1F] bg-opacity-80 flex-col items-center justify-center ${
@@ -144,7 +141,7 @@ const MangaCard = React.memo(({ manga, setIsLastItem = () => { return false } })
               to={`/manga/${manga.id}/overview`}
               className="flex justify-center items-center btn text-white font-bold bg-[#1B6FA8] hover:bg-[#E40066] border border-[#1F1F1F] w-max px-4 py-3 rounded"
             >
-              <span className="z-20"> Read Now</span>
+              <span className="z-20">Read Now</span>
             </Link>
           </div>
         </div>
@@ -181,7 +178,7 @@ const MangaCard = React.memo(({ manga, setIsLastItem = () => { return false } })
             className="mx-auto list-none text-white text-[13px] font-bold bg-[#1B6FA8] hover:bg-[#E40066] border border-[#1F1F1F] w-[80%] px-2 py-2 mb-2 rounded"
             to={`/manga/${manga.id}/overview`}
           >
-            Read Now
+            <span>Read Now</span>
           </Link>
         </div>
         <div className="bg-[#1B6FA8] hidden lg:flex p-2 flex-col items-center justify-center border-y-2 border-[#1F1F1F]">
@@ -203,7 +200,7 @@ const MangaCard = React.memo(({ manga, setIsLastItem = () => { return false } })
               {attributes.tags.slice(0, 2).map((item, index) => (
                 <span
                   key={index}
-                  className="text-[#1F1F1F] text-[12px] overflow-wrap rounded-md py-1 px-2 mr-1 flex justify-center items-center bg-gray-300"
+                  className="text-[#1F1F1F] text-[12px] overflow-wrap rounded-[3px] py-1 px-2 mr-1 flex justify-center items-center bg-gray-300"
                 >
                   {item.attributes.name.en}
                 </span>
@@ -223,13 +220,14 @@ const MangaCard = React.memo(({ manga, setIsLastItem = () => { return false } })
           </div>
           <div className="flex items-center px-2 mb-2">
             <span className="mr-2 text-gray-500">status:</span>
-            <span className="px-2 text-[12px] text-[#1F1F1F] rounded-md p-1 w-min flex justify-center items-center bg-gray-300">
+            <span className="px-2 text-[12px] text-[#1F1F1F] rounded-[3px] p-1 w-min flex justify-center items-center bg-gray-300">
               {attributes.status}
             </span>
           </div>
         </div>
-      </div>
+      </m.div>
     );
-});
+  }
+);
 
 export default MangaCard;
