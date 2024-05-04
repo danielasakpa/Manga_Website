f96e25fc9e10ed2e9cbaf38323df31693ce67cc3
danielasakpa <danielasakpa@gmail.com>
2024-05-04 16:57:51 -0700

    Refactor MangaCards component
    - Utilized useCallback hook to memoize the handleSeeMore function for better performance.
    - Replaced isFetching with isRefetching to match the naming convention.

--- src/components/Manga/MangaCards.js
+++ src/components/Manga/MangaCards.js
@@ -1,53 +1,58 @@
-import { useState, useEffect } from 'react';
-import MangaCard from './MangaCard';
-import MangaCardSkeleton from './MangaCardSkeleton';
-import useMangas from '../../hooks/manga/useMangas';
-import HorizontalScrollMenu from '../HorizontalScrollMenu/HorizontalScrollMenu';
-import SeeMore from '../SeeMore/SeeMore';
+import { useState, useEffect, useCallback } from "react";
+import MangaCard from "./MangaCard";
+import MangaCardSkeleton from "./MangaCardSkeleton";
+import useMangas from "../../hooks/manga/useMangas";
+import HorizontalScrollMenu from "../HorizontalScrollMenu/HorizontalScrollMenu";
+import SeeMore from "../SeeMore/SeeMore";
 
 const MangaCards = ({ type, order, limit, includedTags, excludedTags }) => {
+  const [page, setPage] = useState(0);
+  const { data, isLoading, isError, error, isRefetching } = useMangas(
+    type,
+    order,
+    limit,
+    includedTags,
+    excludedTags,
+    page
+  );
+  const [mangaList, setMangaList] = useState([]);
+  const [isLastItem, setIsLastItem] = useState(false);
 
-    const [page, setPage] = useState(0);
-    const { data, isLoading, isError, error, isFetching } = useMangas(type, order, limit, includedTags, excludedTags, page);
-    const [mangaList, setMangaList] = useState([]);
-    const [isLastItem, setIsLastItem] = useState(false);
+  // Update manga list when new data is loaded
+  useEffect(() => {
+    if (data && data?.data?.length > 0) {
+      setMangaList((prevList) => [...prevList, ...data.data]);
+    }
+  }, [data]);
 
-    // Update manga list when new data is loaded
-    useEffect(() => {
-        if (data && data?.data?.length > 0) {
-            setMangaList(prevList => [...prevList, ...data.data]);
-        }
-    }, [data]);
+  const handleSeeMore = useCallback(() => {
+    // Increment the page when "See More" is clicked
+    setPage((prevPage) => prevPage + 1);
+  }, []);
 
-    const handleSeeMore = () => {
-        // Increment the page when "See More" is clicked
-        setPage(prevPage => prevPage + 1);
-    };
-
-    return (
-        <HorizontalScrollMenu>
-            {isLoading || isError ? (
-                [...Array(20)].map((_, index) => (
-                    <div className="mr-5" key={index} itemID={index} title={index}>
-                        <MangaCardSkeleton />
-                    </div>
-                ))
-            ) : (
-                mangaList.map((manga, i) => (
-                    <div className="mr-5"
-                        key={manga.id + i}
-                        itemID={manga.id + i}
-                        title={manga.id + i}>
-                        <MangaCard
-                            manga={manga}
-                            setIsLastItem={setIsLastItem}
-                        />
-                    </div>
-                ))
-            )}
-            {isLastItem && mangaList.length > 1 && <SeeMore handleSeeMore={handleSeeMore} isRefetching={isFetching} />}
-        </HorizontalScrollMenu>
-    );
+  return (
+    <HorizontalScrollMenu>
+      {isLoading || isError
+        ? [...Array(20)].map((_, index) => (
+            <div className="mr-5" key={index} itemID={index} title={index}>
+              <MangaCardSkeleton />
+            </div>
+          ))
+        : mangaList.map((manga, i) => (
+            <div
+              className="mr-5"
+              key={manga.id + i}
+              itemID={manga.id + i}
+              title={manga.id + i}
+            >
+              <MangaCard manga={manga} setIsLastItem={setIsLastItem} />
+            </div>
+          ))}
+      {isLastItem && mangaList?.length > 1 && (
+        <SeeMore handleSeeMore={handleSeeMore} isRefetching={isRefetching} />
+      )}
+    </HorizontalScrollMenu>
+  );
 };
 
 export default MangaCards;
