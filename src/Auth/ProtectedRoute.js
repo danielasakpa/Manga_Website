0c27c0be9c86d40e2eadbce916e2e0c8bb2bd3b3
danielasakpa <danielasakpa@gmail.com>
2024-05-04 05:24:45 -0700

    perf: used useLocation hook from react router to keep track of the prev route Url

--- src/Auth/ProtectedRoute.js
+++ src/Auth/ProtectedRoute.js
@@ -1,12 +1,13 @@
 // ProtectedRoute.js
 import React from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
 
 const ProtectedRoute = () => {
     const { isAuthenticated } = useAuth();
+    const location = useLocation();
 
-    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" state={{prevUrl: location.pathname}}/>;
 };
 
 export default ProtectedRoute;
