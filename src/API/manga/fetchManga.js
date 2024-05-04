3854ed39e57553509bbaa41293f78b179a9fd4ef
danielasakpa <danielasakpa@gmail.com>
2024-05-03 18:12:26 -0700

    refactor: Update manga fetch endpoint and error handling
    - Updated the proxy server URL to use the new endpoint hosted on Netlify.
    - Modified the manga fetch function to include additional query parameters for fetching manga details.
    - Added error handling to check for undefined response data and throw an error if no data is received.
    - Improved error logging by logging the specific manga ID when an error occurs during fetching.

--- src/API/manga/fetchManga.js
+++ src/API/manga/fetchManga.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
-const PROXY_SERVER_URL = 'https://manga-proxy-server.onrender.com';
+const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
 
 export default async function fetchManga(mangaId) {
-    const response = await axios({
-        method: 'get',
-        url: `${PROXY_SERVER_URL}/api/manga/${mangaId}?includes[]=author&includes[]=artist&includes[]=cover_art`,
-        crossDomain: true,
-        withCredentials: true,
-    });
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `${PROXY_SERVER_URL}/api/v1/manga/${mangaId}?includes[]=author&includes[]=artist&includes[]=cover_art&hasAvailableChapters=true`,
+            crossDomain: true,
+        });
 
-    return response.data.data;
+        const responseData = response.data;
+
+        // Check if response data is undefined
+        if (!responseData || !responseData.data) {
+            throw new Error('No data received from fetchManga');
+        }
+
+        return responseData.data;
+    } catch (error) {
+        console.error(`Error fetching manga ${mangaId}:`, error);
+        throw error; // Rethrow the error to be caught by the caller
+    }
 }
