efaa56b3dbb0a21abe07323b8bbe6aa957775578
danielasakpa <danielasakpa@gmail.com>
2024-05-04 16:49:09 -0700

    create FlagImage component
    - Developed a reusable FlagImage component for displaying flag images.
    - Utilized React Query for efficient data fetching with caching capabilities.
    - Implemented error handling for scenarios where the image fails to load.
    - Utilized dangerouslySetInnerHTML for rendering SVG flag images.
    - Provided customizable props such as alt text, decoding, fetchPriority, loading behavior, and classNames.

---
+++ src/components/FlagImage/FlagImage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const FlagImage = ({ flagCode, className }) => {
+  const PROXY_SERVER_URL = 'https://yuki-proxy-server.netlify.app';
+
+  const {
+    data: flag,
+    isLoading,
+    isError,
+  } = useQuery(
+    ["image", flagCode],
+    async () => {
+      try {
+        const res = await axios.get(
+          `${PROXY_SERVER_URL}/images/flags/${flagCode}`
+        );
+        return res?.data;
+      } catch (error) {
+        throw new Error("Error loading image");
+      }
+    },
+    {
+      // Add variables used inside the query to the list of dependencies
+      enabled: !!flagCode, // Prevents execution if flagCode is falsy
+    }
+  );
+
+  return (
+    <>
+      {isLoading ? (
+        <div className={`${className} animate-pulse bg-gray-200 rounded-sm`} />
+      ) : isError ? (
+        <div className={`${className} animate-pulse bg-gray-200 rounded-sm`} />
+      ) : (
+        <>
+          <div
+            className={className}
+            dangerouslySetInnerHTML={{
+              __html: flag,
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default FlagImage;
