ff82d659016886a66dffa78bb8551929d90f48ce
danielasakpa <danielasakpa@gmail.com>
2024-05-04 23:39:00 -0700

    refector: refector SeeMore component
    - Added onTouchEnd event handler to prevent onClick event from being triggered after onTouchEnd, ensuring better touch support.
    - Extracted handleClick function for improved readability and maintainability.

--- src/components/SeeMore/SeeMore.js
+++ src/components/SeeMore/SeeMore.js
@@ -1,10 +1,19 @@
-import { Oval } from 'react-loader-spinner'
+import { Oval } from 'react-loader-spinner';
 
 const SeeMore = ({ handleSeeMore, isRefetching }) => {
+
+    const handleClick = (e) => {
+        // Prevents onClick event from being triggered after onTouchEnd
+        e.preventDefault(); 
+        // Call handleSeeMore function
+        handleSeeMore();
+    };
+    
     return (
         <div
             className="md:mr-7 flex justify-center items-center bg-white text-black font-medium tracking-[0.3em] hover:bg-[#E40066] hover:text-white cursor-pointer h-[100%] w-[100px] bottom-2 left-14"
-            onClick={handleSeeMore}
+            onTouchEnd={handleSeeMore}
+            onClick={handleClick}
         >
             {
                 isRefetching ?
