f49789586b3fe88a7c86eb6b707d67c27c3e8d56
danielasakpa <danielasakpa@gmail.com>
2024-05-04 23:46:14 -0700

    style: refactor MangaCategories component
    - Changed the layout to use flexbox for better vertical alignment of content.
    - Added rounded corners to the category tags for a more polished appearance.

--- src/components/Manga/MangaCategories.js
+++ src/components/Manga/MangaCategories.js
@@ -3,12 +3,12 @@
 // Manga Categories Component
 const MangaCategories = ({ mangaData }) => {
     return (
-        <div className='mt-6 flex justify-center flex-col'>
+        <div className='flex flex-col justify-center mt-6'>
             <p className='text-[20px]'>Category</p>
             <div className='grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-2 mt-4 w-[100%] md:w-[100%]'>
                 {mangaData?.attributes.tags.slice(0, 10).map((item) => (
                     <div
-                        className={`py-1 px-1 flex items-center justify-center rounded-md font-semibold text-center bg-white text-[10px] md:text-[13px] tracking-[0.1em] text-[#1F1F1F]`}
+                        className={`py-1 px-1 flex items-center justify-center rounded-[4px] font-semibold text-center bg-white text-[10px] md:text-[13px] tracking-[0.1em] text-[#1F1F1F]`}
                         key={item.id}
                     >
                         {item.attributes.name.en.toString()}
