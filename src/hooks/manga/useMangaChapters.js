35a5327a68ada1befcc19fc2152bc9e61d7cb95b
danielasakpa <danielasakpa@gmail.com>
2024-05-04 23:29:45 -0700

    pref: enhance useMangaChapters hook
    - Updated useMangaChapters hook to include additional parameters: limit, page, fetchAll.
    - Added refetch function to allow manual data refetching.
    - Set keepPreviousData option to true to ensure previous data is not discarded during pagination.

--- src/hooks/manga/useMangaChapters.js
+++ src/hooks/manga/useMangaChapters.js
@@ -2,13 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import fetchMangaChapters from '../../API/manga/fetchMangaChapters';
 
 
-const useMangaChapters = (mangaID, languages = ['en']) => {
+const useMangaChapters = (mangaID, languages = ['en'], limit, page, fetchAll) => {
 
-    const { data, isLoading, isError, error } = useQuery(['mangaChapters', mangaID, languages], () => fetchMangaChapters(mangaID, languages));
+    const { data, isLoading, isRefetching, isError, error, refetch } = useQuery(['mangaChapters', mangaID, languages, fetchAll, page], () => fetchMangaChapters(mangaID, languages, limit, page, fetchAll), {
+        keepPreviousData: true, // Ensure previous data is not discarded during pagination
+    });
 
     const total = data?.total ?? 0;
 
-    return { data, isLoading, isError, error, total };
+    return { data, isLoading, isRefetching, isError, error, total, refetch };
 };
 
 export default useMangaChapters;
