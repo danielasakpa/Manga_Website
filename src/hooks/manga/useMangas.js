cd51926a890859faf32d66cc8866ffe2c3a5d52e
danielasakpa <danielasakpa@gmail.com>
2024-05-04 23:27:19 -0700

    pref: enhance useMangas hook
    - Updated useMangas hook to include additional parameters: status.
    - Adjusted prefetching logic to handle potential null values in data array.

--- src/hooks/manga/useMangas.js
+++ src/hooks/manga/useMangas.js
@@ -1,19 +1,19 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchMangas from "../../API/manga/fetchMangas";
 import fetchManga from '../../API/manga/fetchManga';
-import tags from '../../assets/tags';
+import tags from '../../assets/tags/tags';
 
 
-export default function useMangas(type, order, limit, includedTags, excludedTags, page) {
+export default function useMangas(type, order, limit, includedTags, excludedTags, page, status = "") {
     const client = useQueryClient();
 
 
-    return useQuery([type, includedTags, page], () => fetchMangas(tags, order, limit, includedTags, excludedTags, page), {
+    return useQuery([type, order, includedTags, excludedTags, status, page], () => fetchMangas(tags, order, limit, includedTags, excludedTags, page, status), {
         keepPreviousData: true, // Ensure previous data is not discarded during pagination
         onSuccess: async (data) => {
             // Use Promise.all to parallelize fetching manga details
-            await Promise.all(data.data.map(async manga => {
-                const mangaId = manga.id;
+            await Promise.all(data?.data?.map(async manga => {
+                const mangaId = manga?.id;
                 await client.prefetchQuery(['manga', mangaId], () => fetchManga(mangaId));
             }));
         }
