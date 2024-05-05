bd1a52da40223380a929f1acba080f40e3f81e34
danielasakpa <danielasakpa@gmail.com>
2024-05-04 23:53:38 -0700

    refector: refactor Layout component
    - Removed Sidebar component.
    - Added AnimatePresence from framer-motion for future animations.
    - Adjusted layout styles for full height and scrollable content.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,19 @@
 import { Outlet } from "react-router-dom";
-import Nav from '../components/Nav/Nav';
-import Sidebar from "../components/Sidebar/Sidebar";
+import Nav from "../components/Nav/Nav";
+// import Sidebar from "../components/Sidebar/Sidebar";
 import Footer from "../components/Footer/Footer";
-import { ToastContainer, Zoom } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import { AnimatePresence } from "framer-motion";
 
 function Layout() {
-
-    return (
-        <div>
-            <Nav />
-            <div className="flex">
-                <Sidebar />
-                <div className="w-full h-[100%] overflow-x-hidden mx-auto lg:w-4/5">
-                    <Outlet />
-                    <Footer />
-                </div>
-            </div>
-            <ToastContainer newestOnTop />
-        </div >
-    );
+  return (
+    <AnimatePresence>
+      <div className="w-full h-[100vh] overflow-x-hidden overflow-y-scroll mx-auto ">
+        <Nav />
+        <Outlet />
+        <Footer />
+      </div>
+    </AnimatePresence>
+  );
 }
 
 export default Layout;
