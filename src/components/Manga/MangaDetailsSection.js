b8de34e130ad5df83a0d8e547834db04255abbb9
danielasakpa <danielasakpa@gmail.com>
2024-05-05 23:40:43 -0700

    refector: Refactor MangaDetailsSection component
    - Utilized useMemo hook to optimize rendering by memoizing the computed manga details array based on dependencies.

--- src/components/Manga/MangaDetailsSection.js
+++ src/components/Manga/MangaDetailsSection.js
@@ -1,17 +1,18 @@
-
+import { useMemo } from 'react';
 
 // Manga Details Section Component
-const MangaDetailsSection = ({ id, mangaData, statistics, isStatsLoading }) => {
+const MangaDetailsSection = ({ mangaData, statistics, isStatsLoading }) => {
 
     const { rating, follows } = statistics || {};
 
-    const mangaDetails = [
+    const mangaDetails = useMemo(() => [
         { label: 'Status', value: mangaData?.attributes?.status || "No Status" },
         { label: 'Demographic', value: mangaData?.attributes?.publicationDemographic || "No Demographic".slice(0, 6) + "..." },
         { label: 'Year', value: mangaData?.attributes?.year || "No date" },
         { label: 'Rating', value: `${rating?.average?.toString().slice(0, 3)} ⭐` || "No rating" },
         { label: 'follows', value: follows || 0 }
-    ];
+    ], [mangaData, rating, follows]);
+
     return (
         <>
             {
@@ -20,7 +21,7 @@ const MangaDetailsSection = ({ id, mangaData, statistics, isStatsLoading }) => {
                         {mangaDetails.map((detail, index) => (
                             <div
                                 key={index}
-                                className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-x-[1.5px] border-opacity-30 md:mx-[2px] border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
+                                className='text-[10px] md:text-[15px] px-4 md:px-7 py-2 basis-1/3 border-x-[1.5px] border-opacity-30 border-[#1F1F1F] font-medium tracking-[0.1em] lg:tracking-[0.2em] cursor-pointer bg-white text-[#1F1F1F]'
                             >
                                 {
                                     detail.label === "follows" ?
@@ -33,7 +34,7 @@ const MangaDetailsSection = ({ id, mangaData, statistics, isStatsLoading }) => {
                     </div> :
                     <div className="flex flex-wrap justify-center mt-4 text-center xl:flex-nowrap gap-y-1">
                         {[...Array(5)].map((_, index) => (
-                            <div key={index} className="animate-pulse basis-1/3 px-4 py-3.5 md:py-4 w-full md:w-1/3 border-1 md:mx-[2px] border-[#1F1F1F] font-medium tracking-[0.3em] cursor-pointer bg-white text-[#1F1F1F]"></div>
+                            <div key={index} className="animate-pulse basis-1/3 px-4 py-3.5 md:py-4 w-full md:w-1/3 border-x-[1.5px] border-opacity-30 border-[#1F1F1F] font-medium tracking-[0.3em] cursor-pointer bg-white text-[#1F1F1F]"></div>
                         ))}
                     </div>
             }
