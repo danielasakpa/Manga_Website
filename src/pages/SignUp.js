e41e7923f2bdf04aa40934dbe520554c58c94b32
danielasakpa <danielasakpa@gmail.com>
2024-05-06 00:52:30 -0700

    refactor: Refactor SignUp component
    - Updated import paths for images and SVGs to reflect the new directory structure.
    - Replaced the 'BallTriangle' loading spinner with 'TailSpin' from 'react-loader-spinner' for consistency.
    - Adjusted styles and class names to maintain consistency and improve readability.

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import authBackground from "../assets/authBackground.jpg";
-import googleLogo from "../assets/googleLogo.svg";
+import authBackground from "../assets/images/authBackground.jpg";
+import googleLogo from "../assets/svg/googleLogo.svg";
 import { createUser } from "../API/user/user";
 import showToast from '../utils/toastUtils';
-import { BallTriangle } from 'react-loader-spinner'
+import { TailSpin } from 'react-loader-spinner'
 import { useAuth } from "../Auth/AuthProvider";
 
 const SignUp = () => {
@@ -50,10 +50,10 @@ const SignUp = () => {
 
 
   return (
-    <section className="bg-white text-sm md:grid md:grid-cols-2 justify-items-center">
-      <div className=" max-w-sm mx-auto px-4 py-16 ">
+    <section className="text-sm bg-white md:grid md:grid-cols-2 justify-items-center">
+      <div className="max-w-sm px-4 py-16 mx-auto ">
         <div className="text-center mb-9">
-          <h2 className="text text-3xl font-medium mb-4">
+          <h2 className="mb-4 text-3xl font-medium text">
             Welcome <span className="text-[#1B6FA8]">back</span>!
           </h2>
           <p className="text-sm text-gray-500">
@@ -68,7 +68,7 @@ const SignUp = () => {
             <input
               type="text"
               placeholder="Name"
-              className="peer h-full w-full rounded-md border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              className="peer h-full w-full rounded-[3px] border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
               value={username}
               pattern={`^[A-Za-z0-9]{3,16}$`}
               required
@@ -77,7 +77,7 @@ const SignUp = () => {
               onChange={(e) => setUserName(e.target.value)}
             />
             <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">Username should be 3-16 characters and shouldn't include any special character!</span>
-            <label className="absolute peer-placeholder-shown:top-1/2 peer-focus:top-0 top-0 left-3 -translate-y-1/2 bg-white px-1 duration-300">
+            <label className="absolute top-0 px-1 duration-300 -translate-y-1/2 bg-white peer-placeholder-shown:top-1/2 peer-focus:top-0 left-3">
               Name
             </label>
           </div>
@@ -87,7 +87,7 @@ const SignUp = () => {
             <input
               type="email"
               placeholder="Email"
-              className="peer h-full w-full rounded-md border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              className="peer h-full w-full rounded-[3px] border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
               value={email}
               pattern={`[^@ \t\r\n]+@[^@ \t\r\n]+.[^@ \t\r\n]+`}
               required
@@ -96,7 +96,7 @@ const SignUp = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
             <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">It should be a valid email address!</span>
-            <label className="absolute peer-placeholder-shown:top-1/2 peer-focus:top-0 top-0 left-3 -translate-y-1/2 bg-white px-1 duration-300">
+            <label className="absolute top-0 px-1 duration-300 -translate-y-1/2 bg-white peer-placeholder-shown:top-1/2 peer-focus:top-0 left-3">
               Email
             </label>
           </div>
@@ -106,7 +106,7 @@ const SignUp = () => {
             <input
               type="password"
               placeholder="Password"
-              className="peer h-full w-full rounded-md border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              className="peer h-full w-full rounded-[3px] border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
               value={password}
               pattern={`^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`}
               required
@@ -115,7 +115,7 @@ const SignUp = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
             <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!</span>
-            <label className="absolute peer-placeholder-shown:top-1/2 peer-focus:top-0 top-0 left-3 -translate-y-1/2 bg-white px-1 duration-300">
+            <label className="absolute top-0 px-1 duration-300 -translate-y-1/2 bg-white peer-placeholder-shown:top-1/2 peer-focus:top-0 left-3">
               Password
             </label>
           </div>
@@ -125,7 +125,7 @@ const SignUp = () => {
             <input
               type="password"
               placeholder="Confirm Password"
-              className="peer h-full w-full rounded-md border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              className="peer h-full w-full rounded-[3px] border-2 border-gray-500 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
               value={confirmPassword}
               pattern={password}
               onFocus={() => setFocused("true")}
@@ -135,7 +135,7 @@ const SignUp = () => {
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
             <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">Passwords don't match!</span>
-            <label className="absolute peer-placeholder-shown:top-1/2 peer-focus:top-0 top-0 left-3 -translate-y-1/2 bg-white px-1 duration-300">
+            <label className="absolute top-0 px-1 duration-300 -translate-y-1/2 bg-white peer-placeholder-shown:top-1/2 peer-focus:top-0 left-3">
               Confirm Password
             </label>
           </div>
@@ -148,23 +148,32 @@ const SignUp = () => {
 
           {/* Buttons */}
           <button
-            className="h-11 w-full text-center font-semibold text-white bg-[#1B6FA8] hover:bg-[#155580] rounded-lg  mb-5"
+            className="h-11 w-full text-center font-semibold text-white bg-[#1B6FA8] hover:bg-[#155580] rounded-[3px] mb-5"
             onClick={handleSignUp}
           >
-            Sign Up
+            {isLoading ? <TailSpin
+              height="20"
+              width="20"
+              color="#ffffff"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            /> : "Sign Up"}
           </button>
           <button
-            className="h-11 w-full flex gap-2 justify-center items-center border-2 border-gray-400 rounded-lg text-gray-500 mb-4">
+            className="flex items-center justify-center w-full gap-2 mb-4 text-gray-500 border-2 border-gray-400 rounded-[3px] h-11">
             <img
               src={googleLogo}
               alt="google"
-              className="h-6 w-6 inline-block"
+              className="inline-block w-6 h-6"
             />
             <span>Sign in with Google</span>
           </button>
         </form>
 
-        <div className="text-center font-semibold text-gray-600">
+        <div className="font-semibold text-center text-gray-600">
           <p className="mb-3">
             Already have an account?{" "}
             <Link to="/login" className="text-[#1B6FA8]">
@@ -182,28 +191,9 @@ const SignUp = () => {
 
       <img
         src={authBackground}
-        className="h-full w-full"
+        className="w-full h-full"
         alt="auth background"
       />
-      {isLoading && (
-        <>
-          <div
-            className={`flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none`}
-          >
-            <BallTriangle
-              height="80"
-              width="80"
-              radius={5}
-              color="#ffffff"
-              ariaLabel="ball-triangle-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-          <div className={`"opacity-25 fixed inset-0 z-40 bg-black`}></div>
-        </>
-      )}
     </section>
   );
 };
