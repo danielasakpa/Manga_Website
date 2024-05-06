45070168e2a05cd97cea2281ed7b143d16bb4cb6
danielasakpa <danielasakpa@gmail.com>
2024-05-06 01:19:03 -0700

    feat: Created lint configuration file with ESLint rules for React and unused imports.

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import react from 'eslint-plugin-react';
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from 'globals';
+const reactRecommended = require('eslint-plugin-react/configs/recommended');
+
+const lint = [
+  reactRecommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      react,
+      "unused-imports": unusedImports
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default lint
+  
